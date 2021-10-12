5ea3f62ef44348c42dc7fe9b8079d85a69470d68
Saddam Hussain <89402245+Studiomazia@users.noreply.github.com>
2021-10-12 14:39:50 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!p1ai_b373g3bmz-**m@%h9+0_8xm7*41etdbi+t266-mogm08'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'humanity-care-services.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'humanity-care-service.herokuapp.com/']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # During development only
